6fb22e9935f8943bc0fe2bac3ef41ad31d1dbb2e
PSEUDOTENSOR / Jonathan McKinney <pseudotensor@users.noreply.github.com>
2009-07-03 05:22:47 +0000

    updated eosextract2c.sh

--- helmeoscode/eosextract2c.sh
+++ helmeoscode/eosextract2c.sh
@@ -1,21 +1,23 @@
+#!/bin/bash
+# eosextract2c.sh <remotedir> <eos-extension for new files>
+# e.g. eosextract2c.sh remotedirtest1 test1
 # copy over the data to Matlab user
 echo Begin transfering files
-#DIR=/u1/matlab/eosdata/
-DIR=/data/matlab/eosdata2/
+REMOTEDIR=$1
 LOCALDIR=./
-ssh -x matlab@ki-rh42.slac.stanford.edu mkdir -p $DIR
-scp $LOCALDIR/eos.dat $LOCALDIR/eos.head $LOCALDIR/eosparms.head matlab@ki-rh42.slac.stanford.edu:$DIR
+ssh -x matlab@ki-rh42.slac.stanford.edu mkdir -p $REMOTEDIR
+scp $LOCALDIR/eos.dat $LOCALDIR/eos.head $LOCALDIR/eosparms.head matlab@ki-rh42.slac.stanford.edu:$REMOTEDIR
 echo Calling Matlab
 # run matlab script to extract eos in HARM form
-ssh -x matlab@ki-rh42.slac.stanford.edu sh /home/matlab/eosconvertrun.sh $DIR
+ssh -x matlab@ki-rh42.slac.stanford.edu sh /home/matlab/eosconvertrun.sh $REMOTEDIR
 #
 echo End Matlab call
 #
 #
-scp matlab@ki-rh42.slac.stanford.edu:$DIR/eosmonodegen.dat $LOCALDIR/eosmonodegen$1.dat
+scp matlab@ki-rh42.slac.stanford.edu:$REMOTEDIR/eosmonodegen.dat $LOCALDIR/eosmonodegen$2.dat
 #
-scp matlab@ki-rh42.slac.stanford.edu:$DIR/eosnew.dat $LOCALDIR/eosnew$1.dat
-scp matlab@ki-rh42.slac.stanford.edu:$DIR/eosdegennew.dat $LOCALDIR/eosdegennew$1.dat
-scp matlab@ki-rh42.slac.stanford.edu:$DIR/eosnew.head $LOCALDIR/eosnew$1.head
+scp matlab@ki-rh42.slac.stanford.edu:$REMOTEDIR/eosnew.dat $LOCALDIR/eosnew$2.dat
+scp matlab@ki-rh42.slac.stanford.edu:$REMOTEDIR/eosdegennew.dat $LOCALDIR/eosdegennew$2.dat
+scp matlab@ki-rh42.slac.stanford.edu:$REMOTEDIR/eosnew.head $LOCALDIR/eosnew$2.head
 #
 echo End transfering files
