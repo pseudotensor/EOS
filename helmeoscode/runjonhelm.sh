f59affe80327b67fd8917005a6da3b37bdc24a97
PSEUDOTENSOR / Jonathan McKinney <pseudotensor@users.noreply.github.com>
2009-07-03 03:22:41 +0000

    Updated runjonhelm.sh

--- helmeoscode/runjonhelm.sh
+++ helmeoscode/runjonhelm.sh
@@ -1,23 +1,15 @@
-DIR=50rhox50tkx50yex1ynux1h
-#DIR=100rhox50tkx50yex20ynux1h.part6
-#DIR=1test
-#DIR=200x200x1x50x1.ynumethod2
-#DIR=100x50x1x50x20
-#DIR=200x200x1x50x40
-#DIR=200x200x1x50x1
+#~/bin/bash
+# runjonhelm.sh <DATADIR>
+PROG=helmeos.exe
+DATADIR=$1
 #
-PROG=helmeos.$DIR.exe
+DIRSOURCE=`pwd`
+DIRDEST=$1
+mkdir -p $DIRDEST
+cd $DIRDEST
 #
-make clean ; make
-cp helmeos.exe $PROG
-mkdir -p $DIR
-#cp -a eosextract*.sh sheneosinterp.sh gethelmmatlabs.sh copy2grbmodel.sh $DIR/
-#cp -a *.atb $PROG helm_table.dat ls220_guesses.dat sheneos.dat sheneos.head $DIR/
-cd $DIR/
-ln -s ../eosextract*.sh ../sheneosinterp.sh ../gethelmmatlabs.sh ../copy2grbmodel.sh .
-ln -s ../*.atb ../helm_table.dat ../ls220_guesses.dat ../sheneos.dat ../sheneos.head .
-cp -a ../$PROG .
-./$PROG &> output.txt
-cd ..
+ln -s $DIRSOURCE/eosextract*.sh $DIRSOURCE/sheneosinterp.sh $DIRSOURCE/gethelmmatlabs.sh $DIRSOURCE/copy2grbmodel.sh .
+ln -s $DIRSOURCE/*.atb $DIRSOURCE/helm_table.dat $DIRSOURCE/ls220_guesses.dat $DIRSOURCE/sheneos.dat $DIRSOURCE/sheneos.head .
+cp -a $DIRSOURCE/$PROG .
 
 
